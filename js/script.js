ef8925761d3f4a46f45879a71a131fe6683c5c58
Salem Ali <salemali.worku@gmail.com>
2023-05-13 01:55:12 -0300

    2nd Update

--- js/script.js
+++ js/script.js
@@ -18,8 +18,24 @@ window.onscroll = function () {
     let frame = frames[i],
       transform = `translateZ(${zVals[i]}px)`,
       opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
-    frame.setAttribute("style", `transform: ${transform}`);
+    frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
   });
 };
 
 window.scrollTo(0, 1);
+
+let soundButton = document.querySelector(".soundbutton"),
+  audio = document.querySelector(".audio");
+
+soundButton.addEventListener("click", (e) => {
+  soundButton.classList.toggle("paused");
+  audio.paused ? audio.play() : audio.pause();
+});
+
+window.onfocus = function () {
+  soundButton.classList.contains("paused") ? audio.pause() : audio.play();
+};
+
+window.onblur = function () {
+  audio.pause();
+};
